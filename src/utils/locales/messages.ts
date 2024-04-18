3b9c3ba8882d9a5936f506150727c02adffc7efc
hjalbarran <hjalbarran@gmail.com>
2024-04-18 09:02:54 +0200

    first commit

---
+++ src/utils/locales/messages.ts
@@ -0,0 +1,15 @@
+import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import ar from './ar.json';
+import zh from './zh.json';
+
+const messages = {
+    en: en,
+    es,
+    fr: fr,
+    ro: ar,
+    zh: zh
+};
+
+export default messages;
